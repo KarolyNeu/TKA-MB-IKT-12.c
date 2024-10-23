1b529901218f53663bbdbbc28485e143872b39f9
KarolyNeu <toth.karoly.attila@njit.hu>
2024-10-23 20:48:25 +0200

    Változtatások
    Létre hoztam egy új mappát amibe a végleges játék fog belekerülni

--- TKA-MB-IKT-12.c/TKA-MB-IKT-12.c_Lib/Nyul.cs
+++ TKA-MB-IKT-12.c/TKA-MB-IKT-12.c_Lib/Nyul.cs
@@ -12,17 +12,20 @@ namespace TKA_MB_IKT_12.c_Lib
         {
             this.NyulElete = nyulEletero;
         }
-
+        public int NyulElete { get; set; }
         public static void FuFogyasztas(Nyul nyul)
         {
             nyul.NyulElete++;
         }
 
-        public static void PontokCsokkenese(Nyul nyul)
+        public static void KorokCsokkenese(Nyul nyul)
         {
             nyul.NyulElete--;
         }
 
-        public int NyulElete { get; set; }
+        public override void KorokCsokkenese()
+        {
+            NyulElete--;
+        }
     }
 }
