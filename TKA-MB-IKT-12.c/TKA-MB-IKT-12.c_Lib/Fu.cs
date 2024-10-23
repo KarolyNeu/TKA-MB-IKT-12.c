20a9e8b1f7b86a71af99ec48fb491a39466277bb
mengbalazs <113674936+mengbalazs@users.noreply.github.com>
2024-10-23 18:02:10 +0200

    A Fu classba megírtam az eltűnését, növekedését és a csökkenését. A hibás sort kikommenteltem.

--- TKA-MB-IKT-12.c/TKA-MB-IKT-12.c_Lib/Fu.cs
+++ TKA-MB-IKT-12.c/TKA-MB-IKT-12.c_Lib/Fu.cs
@@ -8,5 +8,25 @@ namespace TKA_MB_IKT_12.c_Lib
 {
     internal class Fu
     {
+        public Fu(int hossz)
+        {
+            this.fuHossz = hossz;
+        }
+
+        public static void PontokCsokkenese()
+        {
+        //    hossz--;
+        }
+        public static void FuNovekedese(Fu fu)
+        {
+            fu.fuHossz++;
+        }
+
+        public static void FuEltunese(Fu fu)
+        {
+            fu.fuHossz--;
+        }
+
+        public int fuHossz { get; set; }
     }
 }
