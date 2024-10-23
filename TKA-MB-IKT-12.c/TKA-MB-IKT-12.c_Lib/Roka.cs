f8f1d733e3e51aca753b59e1bea1e2ada8d2e6be
mengbalazs <113674936+mengbalazs@users.noreply.github.com>
2024-10-23 18:20:08 +0200

    Megírtam a Roka classba az életerejét, hogyha öl egy nyuszit, akkor növekedjen az életereje, ha a pontok csökkenek, akkor pedig az életereje is csökkenjen.

--- TKA-MB-IKT-12.c/TKA-MB-IKT-12.c_Lib/Roka.cs
+++ TKA-MB-IKT-12.c/TKA-MB-IKT-12.c_Lib/Roka.cs
@@ -8,5 +8,20 @@ namespace TKA_MB_IKT_12.c_Lib
 {
     public class Roka
     {
+        public Roka(int RokaEletero)
+        {
+            this.RokaElete = RokaEletero;
+        }
+
+        public static void NyulOles(Roka roka)
+        {
+            roka.RokaElete += 3;
+        }
+        public static void PontokCsokkenese(Roka roka)
+        {
+            roka.RokaElete--;
+        }
+        public int RokaElete { get; set; }
     }
+
 }
