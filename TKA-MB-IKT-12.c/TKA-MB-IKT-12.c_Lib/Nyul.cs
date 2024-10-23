dc4713669aa821bec5dca574fe7bf1a5c0cc146c
mengbalazs <113674936+mengbalazs@users.noreply.github.com>
2024-10-23 18:12:22 +0200

    A Nyul classba beírtam a nyúl életerejét, hogyha fogyaszt füvet akkor az életereje növekedjen és azt hogyha a pontok csökkenek akkor az életereje kevesebb legyen.

--- TKA-MB-IKT-12.c/TKA-MB-IKT-12.c_Lib/Nyul.cs
+++ TKA-MB-IKT-12.c/TKA-MB-IKT-12.c_Lib/Nyul.cs
@@ -8,6 +8,21 @@ namespace TKA_MB_IKT_12.c_Lib
 {
     public class Nyul
     {
+        public Nyul(int nyulEletero)
+        {
+            this.NyulElete = nyulEletero;
+        }
 
+        public static void FuFogyasztas(Nyul nyul)
+        {
+            nyul.NyulElete++;
+        }
+
+        public static void PontokCsokkenese(Nyul nyul)
+        {
+            nyul.NyulElete--;
+        }
+
+        public int NyulElete { get; set; }
     }
 }
